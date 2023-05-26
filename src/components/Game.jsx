753e8d4f27b04bc018608bd7ae38aef5e91eb97b
vikvakati <vikvakati@gmail.com>
2023-05-26 16:48:04 -0400

    WIP prevent arrow scroll at bottom for game

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { motion } from "framer-motion";
 import { slideIn } from "../utils/motion";
 import { styles } from "../styles";
 import { SectionWrapper } from "../hoc";
 
-
 function Game() {
+	const iframeRef = useRef(null);
+
+	useEffect(() => {
+		const iframe = iframeRef.current;
+		if (!iframe) return;
+
+		const handleKeyDown = (e) => {
+			const isAtBottom =
+				window.innerHeight + window.pageYOffset >= document.body.offsetHeight;
+			if (
+				isAtBottom &&
+				["ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight"].includes(e.code)
+			) {
+				e.preventDefault();
+			}
+		};
+
+		window.addEventListener("keydown", handleKeyDown);
+		return () => {
+			window.removeEventListener("keydown", handleKeyDown);
+		};
+	}, []);
+
 	return (
 		<div className="xl:mt-12 xl:flex-row flex justify-center gap-10 flex-col items-center">
 			<motion.div
@@ -17,11 +39,12 @@ function Game() {
 				<iframe
 					src="https://chriz001.github.io/Reacteroids/"
 					title="Asteroids Game"
-					className="rounded-2xl h-[600px] w-full flex justify-center"
+					className="rounded-2xl h-[600px] w-full flex justify-center game-iframe"
+					ref={iframeRef}
 				></iframe>
 			</motion.div>
 		</div>
 	);
 }
- 
+
 export default SectionWrapper(Game, "");
